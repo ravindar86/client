d8f9039e19e23dda6a78ae087dc7aa646032b4e9
ravindar86 <ravindar.mca@gmail.com>
2022-02-03 07:15:49 +0530

    Added basic component structures for video streams

---
+++ src/BasicNavigation.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Link, Route } from "react-router-dom/cjs/react-router-dom.min";
+
+const PageOne = () => {
+    return <div>PageOne <Link to="/pageTwo">PageTwo</Link></div>;
+}
+
+const PageTwo = () => {
+    return <div><Link to="/">PageOne</Link> PageTwo </div>;
+}
+
+export const BasicNavigation = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pageTwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+}
