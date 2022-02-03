30a689a8c4ee48b5391ace3189e00530317893b4
ravindar86 <ravindar.mca@gmail.com>
2022-02-03 08:48:08 +0530

    Add Header Component to have headers

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Link, Route } from "react-router-dom/cjs/react-router-dom.min";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
@@ -7,9 +8,10 @@ import StreamShow from "./streams/StreamShow";
 
 export const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
                 <div>
+                    <Header />
                   <Route path="/" exact component={StreamList} />
                   <Route path="/streams/new" exact component={StreamCreate} />
                   <Route path="/streams/edit" exact component={StreamEdit} />
