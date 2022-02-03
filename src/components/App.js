8be8c77c486e5be736c72d012c4debf1d55e80a8
ravindar86 <ravindar.mca@gmail.com>
2022-02-03 07:27:32 +0530

    Creating path for the stream components

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter, Link, Route } from "react-router-dom/cjs/react-router-dom.min";
-
-const PageOne = () => {
-    return <div>PageOne <Link to="/pageTwo">PageTwo</Link></div>;
-}
-
-const PageTwo = () => {
-    return <div><Link to="/">PageOne</Link> PageTwo </div>;
-}
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 export const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/pageTwo" component={PageTwo} />
+                  <Route path="/" exact component={StreamList} />
+                  <Route path="/streams/new" exact component={StreamCreate} />
+                  <Route path="/streams/edit" exact component={StreamEdit} />
+                  <Route path="/streams/delete" exact component={StreamDelete} />
+                  <Route path="/streams/show" exact component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
