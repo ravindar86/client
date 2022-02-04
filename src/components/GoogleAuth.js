9f59ad2aa595888a6ae32437ec71e5432011b86b
ravindar86 <ravindar.mca@gmail.com>
2022-02-04 07:00:14 +0530

    Getting auth instance to determine signin state

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,17 +1,33 @@
 import React from "react";
 
 class GoogleAuth extends React.Component{
+    
+    state = {isSignedIn: null};
+
     componentDidMount(){
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '959103917219-jgeumbjppk9l4fd48p3leorb4qhep06m.apps.googleusercontent.com',
                 scope: 'email'
-            })
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({isSignedIn: this.auth.isSignedIn.get()});
+            });
         });
     }
 
+    renderAuthButton() {
+        if(this.state.isSignedIn===null) {
+            return <div></div>;
+        }else if(this.state.isSignedIn) {
+            return <div>Sign Out</div>;
+        }else{
+            return <div>Sign In</div>;
+        }
+    }
+
     render() {
-        return <div>Google Auth</div>;
+        return <div>{this.renderAuthButton()}</div>;
     }
 }
 
