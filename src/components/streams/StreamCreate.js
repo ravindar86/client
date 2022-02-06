134a8d6b2ab7839a06a9cfac80db391557ef48b0
ravindar86 <ravindar.mca@gmail.com>
2022-02-07 00:58:15 +0530

    displaying error messages with the highlight

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -8,12 +8,24 @@ class StreamCreate extends React.Component{
         return <input {...formProps.input} />
     } */
 
-    renderInput({input, label, meta}) {
+
+    renderError({error, touched}) {
+        if(error && touched){
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
+
+    renderInput = ({input, label, meta}) => {
+        const className=`field ${meta.touched && meta.error ? 'error': ''}`;
         return (
-            <div className="field">
+            <div className={className}>
                  <label>{label}</label>
-                 <input {...input} />
-                 <div>{meta.error}</div>
+                 <input {...input} autoComplete="off" />
+                 <div>{this.renderError(meta)}</div>
             </div>
         );
     }
@@ -28,7 +40,7 @@ class StreamCreate extends React.Component{
 
     render(){
         return (
-            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+            <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
                 <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field name="description" component={this.renderInput} label="Enter Description" />
                 <button className="ui button primary">Submit</button>
