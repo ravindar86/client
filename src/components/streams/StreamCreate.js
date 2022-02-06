cb1e46ce0f8478b3eda196e63406d00550c877fd
ravindar86 <ravindar.mca@gmail.com>
2022-02-06 23:30:14 +0530

    Added Redux form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
+/*const StreamCreate = () => {
     return <div>StreamCreate</div>;
 }
 
-export default StreamCreate;
+export default StreamCreate;*/
+
+class StreamCreate extends React.Component{
+
+  /*  renderInput(formProps){
+        // return <input onChange={formProps.input.onChange} value={formProps.input.value} />;
+        return <input {...formProps.input} />
+    } */
+
+    renderInput({input, label}) {
+        return (
+            <div className="field">
+                 <label>{label}</label>
+                 <input {...input} />
+            </div>
+        );
+    }
+
+  /*  onSubmit(event){
+        event.preventDefault();
+    } */
+
+    onSubmit(formProps) {
+        console.log(formProps);
+    }
+
+    render(){
+        return (
+            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
+                <button className="ui button primary">Submit</button>
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
 
+
 export default combineReducers({
-    authReducer: authReducer
+    authReducer: authReducer,
+    form: formReducer
 });
