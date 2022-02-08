3af70ce08918d331ce286fda666996ece9e7a86e
ravindar86 <ravindar.mca@gmail.com>
2022-02-08 08:17:31 +0530

    Added Action Creators for Fetch, Delete and Edit actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
+import { formValues } from "redux-form";
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT } from "./types";
+import { CREATE_STREAM, DELETE_STREAM, EDIT_STREAM, FETCH_STREAM, FETCH_STREAMS, SIGN_IN, SIGN_OUT } from "./types";
 
 export const signIn = (userId) => {
     return {
@@ -15,5 +16,42 @@ export const signOut = () => {
 };
 
 export const createStream = formValues =>  async dispatch => {
-    streams.post('/streams',formValues);
+    const response = await streams.post('/streams',formValues);
+    dispatch({
+        type: CREATE_STREAM,
+        payload: response.data
+    });    
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({
+        type: FETCH_STREAMS,
+        payload: response.data
+    });
+};
+
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/stream/${id}`);
+    dispatch({
+        type: FETCH_STREAM,
+        payload: response.data
+    });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`, formValues);
+    dispatch({
+        type: EDIT_STREAM,
+        payload: response.data
+    });
+};
+
+export const deleteStream = (id) => async dispatch => {
+    await streams.delete(`/streams/${id}`);
+    dispatch({
+        type: DELETE_STREAM,
+        payload: id
+    });
 };
