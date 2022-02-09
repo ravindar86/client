dbe7d0bc0b1b8769418bd41c877042c46095f3be
ravindar86 <ravindar.mca@gmail.com>
2022-02-09 11:16:36 +0530

    Added StreamReducer for Streams

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
+import streamReducer from "./streamReducer";
 
 
 export default combineReducers({
     authReducer: authReducer,
-    form: formReducer
+    form: formReducer,
+    streamReducer : streamReducer
 });
