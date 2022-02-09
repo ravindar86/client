63c7b70e4ae456cf5fda3abc249870af35c9e9e2
ravindar86 <ravindar.mca@gmail.com>
2022-02-09 11:25:02 +0530

    Added Stream List to list all streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-    return <div>StreamList</div>;
+class StreamList extends React.Component{
+    componentDidMount(){
+        this.props.fetchStreams();
+    }
+
+    renderAdmin(stream) {
+       // console.log(this.props.currentUserId);
+       if(stream.userId === this.props.currentUserId) {
+           return (
+                <div className="right floated content">
+                    <button className="ui button primary">Edit</button>
+                    <button className="ui button negative">Delete</button>
+                </div>
+           );
+       }
+    }
+
+    renderList(){
+      //  console.log(this.props.streams);
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">
+                            {stream.description}
+                        </div>
+                    </div>
+                </div>
+            );
+        }); 
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                    {this.renderList()}
+                </div>
+            </div>
+        );
+         
+    }
 }
 
-export default StreamList;
+const mapStateToProps = (state) => {
+    return {
+        streams: Object.values(state.streamReducer),
+        currentUserId: state.authReducer.userId
+    };
+}
+
+export default connect(mapStateToProps,{fetchStreams})(StreamList);
