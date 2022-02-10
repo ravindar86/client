11e6d146a26e696ddfbcfc003ce3ce8e1fef89ba
ravindar86 <ravindar.mca@gmail.com>
2022-02-10 07:03:09 +0530

    Changed to navigate to stream details when Edit button is clicked

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -21,11 +21,10 @@ class StreamList extends React.Component{
     }
 
     renderAdmin(stream) {
-       // console.log(this.props.currentUserId);
        if(stream.userId === this.props.currentUserId) {
            return (
                 <div className="right floated content">
-                    <button className="ui button primary">Edit</button>
+                    <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
                     <button className="ui button negative">Delete</button>
                 </div>
            );
@@ -33,7 +32,6 @@ class StreamList extends React.Component{
     }
 
     renderList(){
-      //  console.log(this.props.streams);
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
