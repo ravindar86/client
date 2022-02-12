c4f386ffed6057c4b1190e5bc12c9b6e3a81f7d3
ravindar86 <ravindar.mca@gmail.com>
2022-02-12 22:21:50 +0530

    Commit for deleting stream

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Router } from "react-router-dom";
+import { Route, Router } from "react-router-dom";
 import history from "../history";
 import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
@@ -16,7 +16,7 @@ export const App = () => {
                   <Route path="/" exact component={StreamList} />
                   <Route path="/streams/new" exact component={StreamCreate} />
                   <Route path="/streams/edit/:id" exact component={StreamEdit} />
-                  <Route path="/streams/delete" exact component={StreamDelete} />
+                  <Route path="/streams/delete/:id" exact component={StreamDelete} />
                   <Route path="/streams/show" exact component={StreamShow} />
                 </div>
             </Router>
