9fa567f386ba45f6ed870f07daa421252893c6dd
ravindar86 <ravindar.mca@gmail.com>
2022-02-12 13:05:00 +0530

    Passed the action to modal from delete component

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,4 +1,5 @@
 import React from "react";
+import history from "../../history";
 import Modal from "../Modal";
 
 const StreamDelete = () => {
@@ -16,6 +17,7 @@ const StreamDelete = () => {
             header="Delete Stream" 
             content="Are you sure to delete this stream?"
             actions={actions}
+            onDismiss={()=>history.push("/")}
          />
     </div>
     );
