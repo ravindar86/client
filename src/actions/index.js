d4054780b31c6bec03312e4aa2b911651ac81c3e
ravindar86 <ravindar.mca@gmail.com>
2022-02-12 22:21:26 +0530

    Commit for deleting stream

--- src/actions/index.js
+++ src/actions/index.js
@@ -57,4 +57,5 @@ export const deleteStream = (id) => async dispatch => {
         type: DELETE_STREAM,
         payload: id
     });
+    history.push('/');
 };
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,8 +1,58 @@
 import React from "react";
+import { connect } from "react-redux";
 import history from "../../history";
 import Modal from "../Modal";
+import { fetchStream, deleteStream } from "../../actions";
+import { Link } from "react-router-dom";
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    renderActions() {
+        const { id } = this.props.match.params;
+        return (
+            <React.Fragment>
+                <button onClick={() => this.props.deleteStream(id)} className='ui negative button'>Delete</button>
+                <Link to='/' className='ui button'>Cancel</Link>
+            </React.Fragment>
+        );
+    }
+
+    renderContent() {
+        if (!this.props.stream) {
+            return "Are you sure to delete this stream?";
+        }
+
+        return `Are you sure to delete the stream '${this.props.stream.title}'?`;
+    }
+
+    render() {
+        return (
+            <div>
+                <Modal
+                    header="Delete Stream"
+                    content={this.renderContent()}
+                    actions={this.renderActions()}
+                    onDismiss={() => history.push("/")}
+                />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streamReducer[ownProps.match.params.id]
+    };
+};
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
+
+
+/*const StreamDelete = () => {
     const actions = (
         <React.Fragment>
             <button className='ui negative button'>Delete</button>
@@ -23,4 +73,4 @@ const StreamDelete = () => {
     );
 }
 
-export default StreamDelete;
+export default StreamDelete; */
